896725130c2b6cb8301b9af6392110b94ab0776e
abhishek-emergys <abhishek.nalawade@emergys.com>
2024-12-03 00:58:31 +0530

    Implemented Login and Register page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,69 @@
-import Sidebar from "../components/Sidebar"
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      <Sidebar/>
-    </div>
-  )
-}
+    <>
+      <div className="flex flex-col gap-4 items-center mt-10">
+        <div>
+          <img
+            src="public/favicon.png"
+            className="h-16"
+            alt="Meter Wise Logo"
+          />
+        </div>
+        <div className="text-center gap-10 mb-8">
+          <p className="text-3xl font-bold">Sign in to your account</p>
+          <span className="text-blue-500">
+            Or{" "}
+            <Link to="/register">
+              <span className="text-lg font-semibold">
+                create a new user account
+              </span>
+            </Link>
+          </span>
+        </div>
+      </div>
+      <div>
+        <form className="max-w-lg mx-auto shadow-lg border-gray-200 border-solid rounded-lg p-8 border">
+          <div className="mb-5">
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-600 dark:text-white"
+            >
+              Email Address
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="john.doe@gmail.com"
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-600 dark:text-white"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="my-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-auto sm:w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
 
-export default Login
+export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,135 @@
+import { Link } from "react-router-dom";
+
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div>
+      <div className="flex flex-col gap-4 items-center mt-10">
+        <div>
+          <img
+            src="public/favicon.png"
+            className="h-16"
+            alt="Meter Wise Logo"
+          />
+        </div>
+        <div className="text-center gap-10 mb-8">
+          <p className="text-3xl font-bold">Create a new account</p>
+          <span className="text-blue-500">
+            Or{" "}
+            <Link to="/login">
+              <span className="text-lg font-semibold">
+                login to your account
+              </span>
+            </Link>
+          </span>
+        </div>
+      </div>
+      <div>
+        <form className="max-w-2xl mx-auto shadow-lg border-gray-200 border-solid rounded-lg p-8 border">
+          <div className="flex w-full justify-between">
+            <div className="mb-5">
+              <label
+                htmlFor="user-name"
+                className="block mb-2 text-sm font-medium text-gray-600 dark:text-white"
+              >
+                User Name
+              </label>
+              <input
+                type="text"
+                id="user-name"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[22vw] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="John Doe"
+                required
+              />
+            </div>
+            <div className="mb-5">
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-600 dark:text-white"
+              >
+                Email Address
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[22vw] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="john.doe@gmail.com"
+                required
+              />
+            </div>
+          </div>
+          <div className="flex w-full justify-between">
+            <div className="mb-5">
+              <label
+                htmlFor="password"
+                className="block mb-2 text-sm font-medium text-gray-600 dark:text-white"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[22vw] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required
+              />
+            </div>
+            <div className="mb-5">
+              <label
+                htmlFor="confirm-password"
+                className="block mb-2 text-sm font-medium text-gray-600 dark:text-white"
+              >
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                id="confirm-password"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[22vw] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required
+              />
+            </div>
+          </div>
 
-export default Register
+          <div className="flex w-full justify-between">
+            <div className="mb-5">
+              <label
+                htmlFor="address"
+                className="block mb-2 text-sm font-medium text-gray-600 dark:text-white"
+              >
+                Address
+              </label>
+              <input
+                type="text"
+                id="address"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[22vw] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Pune city"
+                required
+              />
+            </div>
+            <div className="mb-5">
+              <label
+                htmlFor="pincode"
+                className="block mb-2 text-sm font-medium text-gray-600 dark:text-white"
+              >
+                Pincode
+              </label>
+              <input
+                type="text"
+                id="pincode"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[22vw] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="411001"
+                required
+              />
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="my-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-auto sm:w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Create account
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
